55f11084bcc9faa3df7e12dc1be057cf19991064
Jonathandujols <jonathandujols@gmail.com>
2022-08-14 10:19:55 +0200

    mentuons légales

--- pages/mentions-legales.tsx
+++ pages/mentions-legales.tsx
@@ -27,14 +27,15 @@ const Home: NextPage = () => {
             <h2>Réalisation</h2>
             <p><strong>Zukah - Agence Digitale</strong><br></br>27 rue du trouilh<br></br>40400 Meilhan<br></br>Tél :<a href="tel:0648182936"> 06 48 18 29 36</a> </p>
             <h2>Hébergement</h2>
-            <p><strong>O2Switch</strong><br></br>222 Boulevard Gustave Flaubert<br></br>63000 Clermont-Ferrand<br></br>Tél :<a href="tel:0444446040"> (+33) 4 44 44 60 40</a> </p>
+            <p><strong>Vercel Inc.</strong><br></br>340 S Lemon Ave #4133<br></br>Walnut, CA 91789</p>
             <h2>Conditions d’utilisation</h2>
             <p>L’utilisation du présent site implique l’acceptation pleine et entière des conditions générales d’utilisation décrites ci-après. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment.</p>
             <h2>Informations</h2>
             <p>Les informations et documents du site sont présentés à titre indicatif, ne revêtent pas un caractère exhaustif, et ne peuvent engager la responsabilité du propriétaire du site.</p>
-            <p>Le propriétaire du site ne peut être tenu responsable des dommages directs et indirects consécutifs à l’accès au site.</p><h2>Interactivité</h2>
+            <p>Le propriétaire du site ne peut être tenu responsable des dommages directs et indirects consécutifs à l’accès au site.</p>
+            {/* <h2>Interactivité</h2>
             <p>Les utilisateurs du site peuvent éventuellement y déposer du contenu, apparaissant sur le site dans des espaces dédiés (notamment via les commentaires). Le contenu déposé reste sous la responsabilité de leurs auteurs, qui en assument pleinement l’entière responsabilité juridique.</p>
-            <p>Le propriétaire du site se réserve le droit de retirer sans préavis et sans justification tout contenu déposé par les utilisateurs qui ne satisferait pas à la charte déontologique du site ou à la législation en vigueur.</p>
+            <p>Le propriétaire du site se réserve le droit de retirer sans préavis et sans justification tout contenu déposé par les utilisateurs qui ne satisferait pas à la charte déontologique du site ou à la législation en vigueur.</p> */}
             <h2>Propriété intellectuelle</h2>
             <p>Sauf mention contraire, tous les éléments accessibles sur le site (textes, images, graphismes, logo, icônes, sons, logiciels, etc.) restent la propriété exclusive de leurs auteurs, en ce qui concerne les droits de propriété intellectuelle ou les droits d’usage.</p>
             <p>Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de l’auteur.</p>
